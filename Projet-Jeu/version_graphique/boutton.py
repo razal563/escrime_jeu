034d8fbd22b1591c38489988812279cb9479cd44
razal563 <119457644+razal563@users.noreply.github.com>
2022-11-29 23:36:15 +0100

    Update boutton.py

--- Projet-Jeu/version_graphique/boutton.py
+++ Projet-Jeu/version_graphique/boutton.py
@@ -1,14 +1,14 @@
-#Cette classe a été trouvé sur github elle permet de créer des boutons et l'interraction avec celle-ci
+#Cette classe permet d'implémenter des boutons avec leur position et l'image qui leur appartient
 import pygame
 
 class Boutton():
-	def __init__(self, x, y, image, scale):
-		width = image.get_width()
-		height = image.get_height()
-		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+	def __init__(self, x, y, image, echelle):
+		largeur = image.get_width()
+		hauteur = image.get_height()
+		self.image = pygame.transform.scale(image, (int(largeur * echelle), int(hauteur * echelle)))
 		self.rect = self.image.get_rect()
-		self.rect.topleft = (x, y)
-		self.clicked = False
+		self.rect.haut_gauche = (x, y)
+		self.cliquer_source = False
   
 	#Cette fonction fais 2 chose, premier chose elle dessine le boutton sur l'ecran 
  	#deuxiement elle cherche la position de la souris la compare si elle cohenside avec le boutton en question
@@ -20,12 +20,12 @@ class Boutton():
 
 		#cherche si la souris est pardessus le bouton et si l'utilisateur clique dessus 
 		if self.rect.collidepoint(pos):
-			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
-				self.clicked = True
+			if pygame.mouse.get_pressed()[0] == 1 and self.cliquer_source == False:
+				self.cliquer_source = True
 				action = True
 
 		if pygame.mouse.get_pressed()[0] == 0:
-			self.clicked = False
+			self.cliquer_source = False
 
 		#dessine un boutton sur l'ecran
 		surface.blit(self.image, (self.rect.x, self.rect.y))
